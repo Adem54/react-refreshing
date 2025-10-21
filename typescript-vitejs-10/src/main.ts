8652d433bdbad7e73081a4bd252ec8859c2535cd
Adem Erbas <ae@netsense.no>
2025-10-21 15:56:05 +0200

    update

---
+++ typescript-vitejs-10/src/main.ts
@@ -0,0 +1,6 @@
+//Vite.js pronounced viit..
+//Javascript, typescript, react, angular,svelte projelerini cok hizli bir sekilde baslatip ayaga kaldiran bir kutuphanedir
+//NODE.JS IIN YUKLENMIS OLMASI GEREKIR,NODE.JS YUKLENINCE NPM OTOMATIK OLARAK YUKLENIR 
+//NODE.JS YUKLEDIIKTENSONRA
+//node -v ile yuklu mu ve hangi versiyon yuklu bunu kontrol edelim..
+//npm create vite@latest=>VAnilla=>Typescript
